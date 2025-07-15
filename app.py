c3d4023725f07fa7c95981bf89f897680bd5651c
RailwayBot <railway@bot.com>
2025-07-15 23:43:56 +0600

    Исправление 40 попытка запуска

--- app.py
+++ app.py
@@ -732,4 +732,4 @@ def hotel_detail_page(index):
 # ===========================
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=port)
