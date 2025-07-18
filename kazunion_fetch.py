fc2f5574169c7337d4e5b78239c5fb8f6ee3fc54
RailwayBot <railway@bot.com>
2025-07-18 16:19:45 +0600

    Исправление 47 попытка запуска

--- kazunion_fetch.py
+++ kazunion_fetch.py
@@ -87,8 +87,6 @@ def remote_trigger():
     threading.Thread(target=background_task).start()
     return "Парсинг запущен", 200
 
-
-port = int(os.environ.get("PORT", 8080))
-app.run(host="0.0.0.0", port=port)
-
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
